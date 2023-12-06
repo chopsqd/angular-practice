1d0a81814b7f29dfa6377045162bdc13eec9285f
Michael Chop <007micle007@gmail.com>
2023-12-06 23:01:20 +0300

    Made the basic layout of the project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {PostPageComponent} from "./post-page/post-page.component";
+
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/polyfills.ts
@@ -0,0 +1,3 @@
+// import 'classlist.js';
+// import 'web-animations-js';
+import 'zone.js/dist/zone';
