2eab6618f25a8fd676350e25eacb18b9ad34302a
Michael Chop <007micle007@gmail.com>
2023-12-09 23:50:11 +0300

    Made the basic layout for Login page

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,12 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {RouterModule} from "@angular/router";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -17,6 +18,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forChild([
             {
                 path: '',
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {IUser} from '../../shared/interfaces';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
 
-  ngOnInit() {
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.email
+            ]),
+            password: new FormControl(null, [
+                Validators.required,
+                Validators.minLength(6)
+            ])
+        })
+    }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+
+    const user: IUser = this.form.value
   }
-
 }
